b3a1ae84aa2521068396f75ef94faa19114e883c
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-05-19 23:43:23 -0300

    [ADAPTO-47] T006 - feat: login activity with basic design and handleLogin func

--- app/src/main/java/br/com/ioasys/camp/adaptoandroid/MainActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/adaptoandroid/MainActivity.kt
@@ -1,11 +1,31 @@
 package br.com.ioasys.camp.adaptoandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val btnLogin: Button by lazy {
+        findViewById(R.id.btnLogin)
+    }
+    lateinit var edtEmail: EditText
+    lateinit var edtPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnLogin.setOnClickListener {
+            handleLogin(edtEmail.text.toString(), edtPassword.text.toString())
+        }
+    }
+
+    private fun handleLogin(email: String, password: String) {
+//        INSERT NAVIGATION TO NEXT ACTIVITY
+        Toast.makeText(this, "Button pressed with email ${email} and password ${password}", LENGTH_LONG).show()
     }
 }
