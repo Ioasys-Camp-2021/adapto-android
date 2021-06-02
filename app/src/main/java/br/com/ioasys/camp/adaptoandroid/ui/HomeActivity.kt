5adc2b74ebd12713668d58045c1194cb9ac216fb
João Pedro Mayrink <joaopmayrinkj@gmail.com>
2021-06-02 00:55:57 -0300

    [ADAPTO-193] feat: create drawer menu component

--- app/src/main/java/br/com/ioasys/camp/adaptoandroid/ui/HomeActivity.kt
+++ app/src/main/java/br/com/ioasys/camp/adaptoandroid/ui/HomeActivity.kt
@@ -1,13 +1,43 @@
 package br.com.ioasys.camp.adaptoandroid.ui
 
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import br.com.ioasys.camp.adaptoandroid.R
 
 class HomeActivity : AppCompatActivity() {
 
+    private val drawerLayout: DrawerLayout by lazy { findViewById(R.id.drawer_layout) }
+    private val btnClose: Button by lazy { findViewById(R.id.btnClose) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val toolbar: Toolbar = findViewById(R.id.tbLoggedHome)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.nav_app_bar_open_drawer_description,
+                R.string.navigation_drawer_close)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+//        btnClose.setOnClickListener {
+//            onBackPressed()
+//        }
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
